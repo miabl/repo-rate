7991f45131c773184cf7a40536cdd144e508afaf
Mia <80946195+miabl@users.noreply.github.com>
2022-05-30 22:40:33 +0800

    signin form test

---
+++ src/components/Form/SignInContainer.jsx
@@ -0,0 +1,70 @@
+import { View, Pressable, StyleSheet } from "react-native"
+import Text from "../Text"
+import { Formik } from "formik"
+import FormikTextInput from "./FormikTextInput"
+import theme from "../../theme"
+import useSignIn from "../../hooks/useSignIn"
+import * as yup from "yup"
+import { useNavigate } from "react-router-native"
+
+const styles = StyleSheet.create({
+  component: {
+    backgroundColor: theme.colors.cardBackground,
+  },
+  button: {
+    margin: 12,
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 3,
+    backgroundColor: theme.colors.primary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+})
+
+const initialValues = {
+  username: "",
+  password: "",
+}
+
+const validationSchema = yup.object().shape({
+  username: yup.string().required("Username is required"),
+  password: yup.string().required("Password is required"),
+})
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View style={styles.component}>
+      <FormikTextInput
+        name="username"
+        placeholder="Username"
+        testID="usernameField"
+      />
+      <FormikTextInput
+        name="password"
+        placeholder="Password"
+        secureTextEntry={true}
+        testID="passwordField"
+      />
+      <Pressable onPress={onSubmit} style={styles.button}>
+        <Text fontWeight="bold" color="top" testID="submitButton">
+          Sign In
+        </Text>
+      </Pressable>
+    </View>
+  )
+}
+
+const SignInContainer = ({ initialValues, onSubmit }) => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  )
+}
+
+export default SignInContainer
